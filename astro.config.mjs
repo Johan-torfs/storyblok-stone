4cffd42eced562e19a3fb88ed65d339a137c25d5
Johan Torfs <j.t.johan.torfs@gmail.com>
2023-09-15 16:01:13 +0200

    dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,27 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
+
+const env = loadEnv("", process.cwd(), ['STORYBLOK', 'PUBLIC_VERCEL_ENV', ]);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: env.PUBLIC_VERCEL_ENV === 'production' ? 'static' : 'server',
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
+      apiOptions: {},
+      // storyblok-js-client options
+      components: {
+      },
+      componentsDir: "src",
+      enableFallbackComponent: true,
+      customFallbackComponent: "",
+      useCustomApi: false
+    })
+  ],
+});
